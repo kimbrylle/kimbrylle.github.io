162dfbe90c05fe72234936b9b61a29ef36a4fa7d
Kim Brylle M. Espera <esperakb@gmail.com>
2016-09-28 12:23:13 +0800

    Update
    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,5 @@
-/**
- * the semi-colon before the function invocation is a safety
- * net against concatenated scripts and/or other plugins
- * that are not closed properly.
-
- * undefined is used here as the undefined global
- * variable in ECMAScript 3 and is mutable (i.e. it can
- * be changed by someone else). undefined isn't really
- * being passed in so we can ensure that its value is
- * truly undefined. In ES5, undefined can no longer be
- * modified.
-
- * window and document are passed through as local
- * variables rather than as globals, because this (slightly)
- * quickens the resolution process and can be more
- * efficiently minified (especially when both are
- * regularly referenced in our plugin).
- */
-
-;(function(window, document, $, undefined) {
+;
+(function(window, document, $, undefined) {
 
 	'use strict';
 
@@ -25,9 +7,15 @@
 		'active': '#about'
 	});
 
-	$('#contact .row').contents().filter(function() { return this.nodeType === 3; }).remove();
-	$('.site-list').contents().filter(function() { return this.nodeType === 3; }).remove();
-	$('.skills-list').contents().filter(function() { return this.nodeType === 3; }).remove();
+	$('#contact .row').contents().filter(function() {
+		return this.nodeType === 3;
+	}).remove();
+	$('.site-list').contents().filter(function() {
+		return this.nodeType === 3;
+	}).remove();
+	$('.skills-list').contents().filter(function() {
+		return this.nodeType === 3;
+	}).remove();
 
 	$('#btn-email').on('click', function() {
 		_gaq.push(['_trackEvent', 'Email', 'Click', 'Header']);
@@ -45,16 +33,16 @@
 			// IE 10 or older => return version number
 			var iever = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
 			// return version number if lesser than ver 9
-			if(iever < 9)
+			if (iever < 9)
 				return iever;
 		}
 		// other browser
 		return false;
 	}
-	if(isIE()) {
+	if (isIE()) {
 		var ie_class = '';
-		for(var y=parseInt(isIE()); y<9 ;y++)
-			ie_class = ie_class + 'lt-ie' + (y+1) + ' ';
+		for (var y = parseInt(isIE()); y < 9; y++)
+			ie_class = ie_class + 'lt-ie' + (y + 1) + ' ';
 		$('html').addClass(ie_class);
 	}
 	/* END - IE Hack Fix */
